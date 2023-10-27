522663575ca6d0da5278cc271ddd737a50a2aa62
dotzenith <sharmadanshu01@gmail.com>
2023-10-26 22:20:19 -0400

    mvp

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
 mod api;
 mod parse;
+mod stations;
 
 use crate::api::SeptaApi;
 use crate::parse::Parse;
 
 fn main() {
     let api = SeptaApi::new();
-    // println!("{:?}", api.next_to_arrive("30th Street Station", "Cornwells Heights", None).unwrap()[0]);
-    // println!("{:?}", api.arrivals("30th Street Station", None).unwrap());
-    // println!("{:?}", api.train_schedule("731").unwrap()[0]);
-    println!("{}", api.next_to_arrive("30th Street Station", "Cornwells Heights", Some(5)).unwrap().parse()[0]);
+
+    // let result = api.next_to_arrive("30th Street Station", "Cornwells Heights", Some(5));
+    // match result {
+    //     Ok(res) => {
+    //         for train in res.parse().iter() {
+    //             println!("{train}");
+    //         }
+    //     },
+    //     Err(_) => println!("Something errored out"),
+    // };
+
+    let result = api.arrivals("30th Street Station", Some(5));
+    match result {
+        Ok(res) => {
+            for train in res.parse().iter() {
+                println!("{train}");
+            }
+        },
+        Err(_) => println!("Something errored out"),
+    };
+
+    // let result = api.train_schedule("729");
+    // match result {
+    //     Ok(res) => {
+    //         for train in res.parse().iter() {
+    //             println!("{train}");
+    //         }
+    //     },
+    //     Err(_) => println!("Something errored out"),
+    // };
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,8 +1,29 @@
 use std::collections::HashMap;
 
-pub type NextToArriveResult = Vec<HashMap<String, Option<String>>>;
-pub type ArrivalResult = HashMap<String, Vec<HashMap<String, Vec<HashMap<String, Option<String>>>>>>;
-pub type TrainScheduleResult = Vec<HashMap<String, Option<String>>>;
+pub struct NextToArriveResult(pub Vec<HashMap<String, Option<String>>>);
+pub struct ArrivalResult(
+    pub HashMap<String, Vec<HashMap<String, Vec<HashMap<String, Option<String>>>>>>,
+);
+pub struct TrainScheduleResult(pub Vec<HashMap<String, Option<String>>>);
+
+pub fn parse_time(time: Option<&str>) -> String {
+    if time.is_none() {
+        return "None".to_owned();
+    }
+
+    let time_vec: Vec<&str> = time.unwrap().split(" ").collect::<Vec<&str>>()[1]
+        .split(":")
+        .collect();
+    let mut hour = time_vec[0].parse::<u8>().unwrap_or(0);
+    let minute = time_vec[1].parse::<u8>().unwrap_or(0);
+    let mut meridian = "AM";
+
+    if hour > 12 {
+        hour -= 12;
+        meridian = "PM";
+    }
+    format!("{:02}:{:02} {}", hour, minute, meridian)
+}
 
 pub trait Parse {
     fn parse(&self) -> Vec<String>;
@@ -10,17 +31,74 @@ pub trait Parse {
 
 impl Parse for NextToArriveResult {
     fn parse(&self) -> Vec<String> {
-        let mut results: Vec<String> = Vec::new();
-        for train in self.iter() {
-            results.push(format!(
-                "{:<11}{:<13}{:<11}{:<9}{}",
-                train["orig_train"].as_ref().unwrap(),
-                train["orig_departure_time"].as_ref().unwrap(),
-                train["orig_arrival_time"].as_ref().unwrap(),
-                train["orig_delay"].as_ref().unwrap(),
-                train["orig_line"].as_ref().unwrap()
-            ));
-        }
-        results
+        self.0
+            .iter()
+            .map(|train| {
+                format!(
+                    "{:<11}{:<13}{:<11}{:<9}{}",
+                    train["orig_train"].as_deref().unwrap_or("balls"),
+                    train["orig_departure_time"].as_deref().unwrap_or("balls"),
+                    train["orig_arrival_time"].as_deref().unwrap_or("balls"),
+                    train["orig_delay"].as_deref().unwrap_or("balls"),
+                    train["orig_line"].as_deref().unwrap_or("balls")
+                )
+            })
+            .collect()
+    }
+}
+
+impl Parse for ArrivalResult {
+    fn parse(&self) -> Vec<String> {
+        let vec = self.0.values().next().unwrap();
+        let north = &vec[0]["Northbound"];
+        let south = &vec[1]["Southbound"];
+
+        let mut trains: Vec<String> = north
+            .into_iter()
+            .map(|train| {
+                format!(
+                    "{:<13}{:<11}{:<27}{:<12}{:<10}{}",
+                    "North",
+                    train["train_id"].as_deref().unwrap_or("balls"),
+                    train["next_station"].as_deref().unwrap_or("balls"),
+                    parse_time(train["sched_time"].as_deref()),
+                    train["status"].as_deref().unwrap_or("balls"),
+                    train["destination"].as_deref().unwrap_or("balls")
+                )
+            })
+            .collect();
+
+        trains.append(
+            &mut south.into_iter()
+            .map(|train| {
+                format!(
+                    "{:<13}{:<11}{:<27}{:<12}{:<10}{}",
+                    "South",
+                    train["train_id"].as_deref().unwrap_or("balls"),
+                    train["next_station"].as_deref().unwrap_or("balls"),
+                    parse_time(train["sched_time"].as_deref()),
+                    train["status"].as_deref().unwrap_or("balls"),
+                    train["destination"].as_deref().unwrap_or("balls")
+                )
+            })
+            .collect::<Vec<String>>()
+        );
+        trains
+    }
+}
+
+impl Parse for TrainScheduleResult {
+    fn parse(&self) -> Vec<String> {
+        self.0
+            .iter()
+            .map(|train| {
+                format!(
+                    "{:<27}{:<18}{}",
+                    train["station"].as_deref().unwrap_or("balls"),
+                    train["sched_tm"].as_deref().unwrap_or("balls"),
+                    train["act_tm"].as_deref().unwrap_or("balls")
+                )
+            })
+            .collect()
     }
 }
